f956ac83265470553935be6a1b90557e9bfbb132
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-10-23 22:06:32 +0200

    in process4

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+
+let btn = $('#button-arrow');
+
+$(window).scroll(function() {
+
+  if ($(window).scrollTop() > 300) {
+
+    btn.addClass('show');
+
+  } else {
+    
+    btn.removeClass('show');
+  }
+
+});
+
+btn.on('click', function(e) {
+
+  e.preventDefault();
+
+  $('html, body').animate({scrollTop:0}, '300');
+
+});
